b89293c69b2dc7e04b4ff348a12d59fa17228b08
dpappas <Zrb6.NCC>
2018-05-16 21:18:02 +0300

    message

---
+++ fix_the_data_using_sentences.py
@@ -0,0 +1,197 @@
+
+import re
+import cPickle as pickle
+from pprint import pprint
+from collections import Counter
+from nltk.tokenize import sent_tokenize
+
+def remove_parenthesis(text):
+    while(True):
+        l       = len(text)
+        text    = re.sub('\(.*?\)','',text)
+        if(l==len(text)):
+            break
+    return text
+
+def create_data(fpath1, fpath2, discard_parenthesis = True):
+    ret     = {}
+    with open(fpath1) as f:
+        m = 0
+        for l in f.readlines():
+            if(m>0):
+                t = l.strip().split('||')
+                ret[t[0]]           = {}
+                text                = t[1].strip().decode('utf-8')
+                if(discard_parenthesis):
+                    text            = remove_parenthesis(text)
+                ret[t[0]]['text']   = get_sents(text)
+                # pprint(ret[t[0]]['text'])
+                # exit()
+            m+=1
+            print m
+        f.close()
+    with open(fpath2) as f:
+        m = 0
+        for l in f.readlines():
+            if(m>0):
+                t = l.strip().split(',')
+                ret[t[0]]['gene']      = t[1].decode('utf-8')
+                ret[t[0]]['variation'] = t[2].decode('utf-8')
+                if(len(t)>3):
+                    ret[t[0]]['class'] = int(t[3])
+            m+=1
+            print m
+        f.close()
+    return ret
+
+def get_the_vocab(data, min_freq):
+    vocab           = Counter()
+    gene_ids        = []
+    variation_ids   = []
+    for item in data.values():
+        text                = ' '.join(item['text'])
+        text                = bioclean(text)
+        text                = re.sub('\d', 'D', text)
+        vocab.update(Counter(text.split()))
+        gene_ids.append(item['gene'])
+        variation_ids.append(item['variation'])
+    gene_ids        = ['PAD', 'UNKN'] + list(set(gene_ids))
+    gene_ids        = dict(zip(gene_ids, range(len(gene_ids))))
+    variation_ids   = ['PAD', 'UNKN'] +list(set(variation_ids))
+    variation_ids   = dict(zip(variation_ids, range(len(variation_ids))))
+    vocab           = Counter(dict([item for item in vocab.items() if (item[1] >= min_freq)]))
+    return vocab, gene_ids, variation_ids
+
+def get_the_chars(vocab):
+    chars = []
+    for k in vocab.keys():
+        chars.extend(k)
+    chars = sorted(list(set(chars)))
+    return chars
+
+def get_ids(vocab, chars):
+    vocab_ids           = dict(zip( sorted(vocab.keys()) , range(2,len(vocab)+2) ))
+    vocab_ids['PAD']    = 0
+    vocab_ids['UNKN']   = 1
+    #
+    char_ids            = dict(zip( sorted(chars) , range(2,len(vocab)+2) ))
+    char_ids['PAD']    = 0
+    char_ids['UNKN']   = 1
+    return vocab_ids, char_ids
+
+def get_token_id(vocab_ids, token):
+    try:
+        return vocab_ids[token]
+    except:
+        return vocab_ids['UNKN']
+
+def first_alpha_is_upper(sent):
+    specials = [
+        '__EU__','__SU__','__EMS__','__SMS__','__SI__',
+        '__ESB','__SSB__','__EB__','__SB__','__EI__',
+        '__EA__','__SA__','__SQ__','__EQ__','__EXTLINK',
+        '__XREF','__URI', '__EMAIL','__ARRAY','__TABLE',
+        '__FIG','__AWID','__FUNDS'
+    ]
+    for special in specials:
+        sent = sent.replace(special,'')
+    for c in sent:
+        if(c.isalpha()):
+            if(c.isupper()):
+                return True
+            else:
+                return False
+    return False
+
+def ends_with_special(sent):
+    sent = sent.lower()
+    ind = [item.end() for item in re.finditer('[\W\s]sp.|[\W\s]nos.|[\W\s]sp.[\W\s]no.|[\W\s][vols.|[\W\s]cv.|[\W\s]e.g.|[\W\s]et[\W\s]al.|[\W\s]i.e.|[\W\s]p.p.m.|[\W\s]cf.|[\W\s]n.a.|[\W\s]no.|[\W\s]fig.|[\W\s]figs.', sent)]
+    if(len(ind)==0):
+        return False
+    else:
+        ind = max(ind)
+        if (len(sent) == ind):
+            return True
+        else:
+            return False
+
+def split_sentences2(text):
+    sents = [l.strip() for l in sent_tokenize(text)]
+    ret = []
+    i = 0
+    while (i < len(sents)):
+        sent = sents[i]
+        while (
+            ((i + 1) < len(sents)) and
+            (
+                ends_with_special(sent)        or
+                not first_alpha_is_upper(sents[i+1])
+                # sent[-5:].count('.') > 1       or
+                # sents[i+1][:10].count('.')>1   or
+                # len(sent.split()) < 2          or
+                # len(sents[i+1].split()) < 2
+            )
+        ):
+            sent += ' ' + sents[i + 1]
+            i += 1
+        ret.append(sent.replace('\n',' ').strip())
+        i += 1
+    return ret
+
+def get_sents(ntext):
+    sents = []
+    for subtext in ntext.split('\n'):
+        subtext = re.sub( '\s+', ' ', subtext.replace('\n',' ') ).strip()
+        if (len(subtext) > 0):
+            ss = split_sentences2(subtext)
+            sents.extend([ s for s in ss if(len(s.strip())>0)])
+    if(len(sents[-1]) == 0 ):
+        sents = sents[:-1]
+    return sents
+
+bioclean = lambda t: ' '.join(re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower()).split())
+
+datadir     = '/home/dpappas/.kaggle/competitions/msk-redefining-cancer-treatment/'
+fpath1      = datadir + 'training_text'
+fpath2      = datadir + 'training_variants'
+train_data  = create_data(fpath1, fpath2, True)
+fpath1      = datadir + 'test_text'
+fpath2      = datadir + 'test_variants'
+test_data   = create_data(fpath1, fpath2, True)
+vocab, gene_ids, variation_ids = get_the_vocab(train_data, 10)
+chars       = get_the_chars(vocab)
+vocab_ids, char_ids = get_ids(vocab, chars)
+print(len(vocab))
+# pprint(vocab.most_common(10))
+# pprint(list(reversed(vocab.most_common()[-10:])))
+# print len(chars)
+# print(''.join(chars))
+# pprint(vocab_ids)
+# pprint(char_ids)
+
+pprint(test_data.items()[0])
+
+pickle.dump(variation_ids,  open('variation_ids.p','wb'))
+pickle.dump(gene_ids,       open('gene_ids.p','wb'))
+pickle.dump(vocab_ids,      open('vocab_ids.p','wb'))
+pickle.dump(char_ids,       open('char_ids.p','wb'))
+pickle.dump(test_data,      open('test_data.p','wb'))
+pickle.dump(train_data,     open('train_data.p','wb'))
+
+
+# for key in test_data:
+#     text                        = test_data[key]['text']
+#     test_data[key]['token_ids'] = [ get_token_id(vocab_ids, token) for token in text.split() ]
+#     test_data[key]['char_ids']  = [ [ get_token_id(char_ids, c) for c in token ] for token in text.split() ]
+#
+# for key in train_data:
+#     text                            = train_data[key]['text']
+#     train_data[key]['token_ids']    = [ get_token_id(vocab_ids, token) for token in text.split() ]
+#     train_data[key]['char_ids']     = [ [ get_token_id(char_ids, c) for c in token ] for token in text.split() ]
+#
+# pprint(train_data.items()[0])
+
+
+
+
+
