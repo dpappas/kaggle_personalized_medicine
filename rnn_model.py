8409b8248075ab14e7242ae37a4792e6dcb585fe
dpappas <Zrb6.NCC>
2018-05-17 00:06:25 +0300

    message

--- rnn_model.py
+++ rnn_model.py
@@ -127,7 +127,7 @@ for e in range(10):
     for f in fs[:-5]:
         d = pickle.load(open(diri + f, 'rb'))
         optimizer.zero_grad()
-        cost_       = model(d['sent_ids'][:,:50], d['targets']-1)
+        cost_       = model(d['sent_ids'][:,:100], d['targets']-1)
         cost_.backward()
         optimizer.step()
         the_cost    = cost_.cpu().data.numpy()[0]
